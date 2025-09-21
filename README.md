32e5e7c3f75f96b9efa96c026d045fb0830ce256
Eskielidder <eskielidder@gmail.com>
2025-09-21 17:12:34 +0800

    CH2 L4 update

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ boot.dev Asteroids
 #git commands
 #clone from github
 git clone https://github.com/eskielidder/<your_repo>
+
+#add all changed files to commit
+git add .
+
+#commit changes
+git merge -m "<commit comment>"
+
+#push changes to github (origin)
+git push origin main
+
+#activate uv environment
+source .venv/bin/activate
---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
