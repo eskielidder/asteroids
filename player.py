e679468882d4c905bc23f1fd40d68e66959074a4
Eskielidder <eskielidder@gmail.com>
2025-10-10 22:25:08 +0800

    CH3-L3 Shooting

--- player.py
+++ player.py
@@ -33,6 +33,13 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
